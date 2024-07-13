d447a4082a09c6d30489fa71385b8bd09b675d42
kshakib22 <59932906+kshakib22@users.noreply.github.com>
2024-07-14 03:46:01 +0600

    same as last commit

--- quartz/components/ContentMeta.tsx
+++ quartz/components/ContentMeta.tsx
@@ -10,7 +10,7 @@ interface ContentMetaOptions {
   /**
    * Whether to display reading time
    */
-  showReadingTime: boolean
+  showReadingTime: false
   showComma: boolean
 }
 
@@ -34,13 +34,13 @@ export default ((opts?: Partial<ContentMetaOptions>) => {
       }
 
       // Display reading time if enabled
-      // if (options.showReadingTime) {
-      //   const { minutes, words: _words } = readingTime(text)
-      //   const displayedTime = i18n(cfg.locale).components.contentMeta.readingTime({
-      //     minutes: Math.ceil(minutes),
-      //   })
-      //   segments.push(displayedTime)
-      // }
+      if (options.showReadingTime) {
+        const { minutes, words: _words } = readingTime(text)
+        const displayedTime = i18n(cfg.locale).components.contentMeta.readingTime({
+          minutes: Math.ceil(minutes),
+        })
+        segments.push(displayedTime)
+      }
 
       const segmentsElements = segments.map((segment) => <span>{segment}</span>)
 
